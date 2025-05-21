a67f46cccb0884de6c2ede4a95e071bac612afe2
afanasevrev <rewoly@mail.ru>
2025-05-21 13:13:40 +0900

    Проект магазин парфюмерии

---
+++ src/test/java/com/example/parfume_shop/ParfumeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parfume_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParfumeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
